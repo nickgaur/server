afcbbb29d0eb38f9914af0854965818161ffca86
nickgaur <nikhilgaur3@rocketmail.com>
2022-07-09 18:00:02 +0530

    database connection created

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 const express = require('express')
 const mysql = require('mysql')
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+})
+// CREATING CONNECTION
+db.connect((err) => {
+    if (err) {
+        throw err
+    }
+    else {
+        console.log("DATABASE CONNECTED!!")
+    }
+});
+
 const app = express()
 
-app.listen(8000, () => {
-    console.log("SERVER STARTED AT " + PORT)
+
+const port = process.env.PORT || process.env.SERVER_PORT
+app.listen(port, () => {
+    console.log("SERVER STARTED AT " + port)
 })
